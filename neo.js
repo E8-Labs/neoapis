8b3dc551cae4941d3dd85411f29326defe4aaaea
salmane8-labs <154466106+salmane8-labs@users.noreply.github.com>
2024-07-25 23:02:30 +0500

    some updates

--- neo.js
+++ neo.js
@@ -35,20 +35,20 @@ app.use((req, res, next) => {
   });
   
   //'https://neo-ai-nu.vercel.app',// my project
-  // app.use(cors({
-  //   origin: 'https://neoai-ebon.vercel.app',
-  //   methods: ['GET', 'POST'],
-  //   allowedHeaders: ['Content-Type', 'Authorization'],
-  //   credentials: true
-  // }));
+  app.use(cors({
+    origin: 'https://neoai-ebon.vercel.app',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+  }));
   
-  // app.options('*', (req, res) => {
-  //   res.header('Access-Control-Allow-Origin', 'https://neoai-ebon.vercel.app');
-  //   res.header('Access-Control-Allow-Methods', 'GET, POST');
-  //   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  //   res.header('Access-Control-Allow-Credentials', 'true');
-  //   res.sendStatus(200);
-  // });
+  app.options('*', (req, res) => {
+    res.header('Access-Control-Allow-Origin', 'https://neoai-ebon.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.sendStatus(200);
+  });
 
   app.use((err, req, res, next) => {
     console.error(err.stack);
