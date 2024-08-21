b6212f48266196807213631f4b04a407e7a7189d
salmane8-labs <154466106+salmane8-labs@users.noreply.github.com>
2024-08-21 10:39:33 +0500

    some updates

--- neo.js
+++ neo.js
@@ -34,17 +34,17 @@ app.use((req, res, next) => {
     next();
   });
 
-  
+  //This works now.
   // 'https://neo-ai-nu.vercel.app',// my project
   app.use(cors({
-    origin: 'https://neoai-ebon.vercel.app',
+    origin: 'http://localhost:3000',//https://neoai-ebon.vercel.app
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
   }));
   
   app.options('*', (req, res) => {
-    res.header('Access-Control-Allow-Origin', 'https://neoai-ebon.vercel.app');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header('Access-Control-Allow-Methods', 'GET, POST');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
