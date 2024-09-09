4c84c5a0e889ac08299ef76e3b151c0f4b47235e
salmane8-labs <154466106+salmane8-labs@users.noreply.github.com>
2024-09-09 13:22:06 +0500

    server update

--- neo.js
+++ neo.js
@@ -36,36 +36,36 @@ app.use((req, res, next) => {
 
  
   // 'https://neo-ai-nu.vercel.app',// my project
-  // app.use(cors({
-  //   origin: 'http://localhost:3000',//https://neoai-ebon.vercel.app
-  //   methods: ['GET', 'POST'],
-  //   allowedHeaders: ['Content-Type', 'Authorization'],
-  //   credentials: true
-  // }));
-  
-  // app.options('*', (req, res) => {
-  //   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  //   res.header('Access-Control-Allow-Methods', 'GET, POST');
-  //   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  //   res.header('Access-Control-Allow-Credentials', 'true');
-  //   res.sendStatus(200);
-  // });
-
-
   app.use(cors({
-    origin: 'https://neoai-ebon.vercel.app',//https://neoai-ebon.vercel.app
+    origin: 'http://localhost:3000',//https://neoai-ebon.vercel.app
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
   }));
   
   app.options('*', (req, res) => {
-    res.header('Access-Control-Allow-Origin', 'https://neoai-ebon.vercel.app');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header('Access-Control-Allow-Methods', 'GET, POST');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.sendStatus(200);
   });
+
+
+  // app.use(cors({
+  //   origin: 'https://neoai-ebon.vercel.app',//https://neoai-ebon.vercel.app
+  //   methods: ['GET', 'POST'],
+  //   allowedHeaders: ['Content-Type', 'Authorization'],
+  //   credentials: true
+  // }));
+  
+  // app.options('*', (req, res) => {
+  //   res.header('Access-Control-Allow-Origin', 'https://neoai-ebon.vercel.app');
+  //   res.header('Access-Control-Allow-Methods', 'GET, POST');
+  //   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  //   res.header('Access-Control-Allow-Credentials', 'true');
+  //   res.sendStatus(200);
+  // });
 
   app.use((err, req, res, next) => {
     console.error(err.stack);
